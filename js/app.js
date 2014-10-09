410df6179515b16458baa0eaa7df0fe3cc909990
pberkman <patriciaberkman@Patricias-MacBook-Pro.local>
2014-10-09 10:54:01 -0400

    initial

--- js/app.js
+++ js/app.js
@@ -2,23 +2,22 @@
 $(document).ready(function(){
 	
 	/*--- Display information modal box ---*/
-  	$(".what").click(function(){
-    	$(".overlay").fadeIn(1000);
+	$(".what").click(function(){
+		$(".overlay").fadeIn(1000);
+	});
 
-  	});
-
-  	/*--- Hide information modal box ---*/
-  	$("a.close").click(function(){
-  		$(".overlay").fadeOut(1000);
-  	});
-  	
+	/*--- Hide information modal box ---*/
+	$("a.close").click(function(){
+		$(".overlay").fadeOut(1000);
+	});
+	
   
 	/*--- Variables ---*/
     var secretNum;
 	var numGuess=0;
 	var userGuess=+$("#userGuess").val();
 	var feedback=$("#feedback");
-	var difference=Math.abs(userGuess-secretNum);	
+	var difference=Math.abs(userGuess-secretNum);
 
 	/*---Functions---*/
 		/*---reset count, guess history and input value---*/
@@ -27,46 +26,48 @@ $(document).ready(function(){
 			$("#count").text(numGuess);
 			$(".guessHistory").remove();
 			$("#userGuess").val("");
-		}
+		};
+
 		/*---generate random #---*/
 		var random=function() {
 			secretNum=Math.floor((Math.random()*100)+1);
-		}
+		};
 		var guessingGame=function() {
-	  			if (userGuess===secretNum) {
-	  				feedback.text("Congratulations! You guessed correctly! Click on '+New Game' to play again!");
-		  		} else if (difference>=50) {
-		  			feedback.text("Ice Cold!");
-		  		} else if (difference>=30 && difference<50) {
-		  			feedback.text("Cold!");
-		  		} else if (difference>=20 && difference<30) {
-		  			feedback.text("Warm!");
-		  		} else if (difference>=10 && difference<20) {
-		  			feedback.text("Hot!");
-		  		} else {
-		  			feedback.text("Very Hot!");
-		  		}
+			if (userGuess===secretNum) {
+				feedback.text("Congratulations! You guessed correctly! Click on '+New Game' to play again!");
+			} else if (difference>=50) {
+				feedback.text("Ice Cold!");
+			} else if (difference>=30 && difference<50) {
+				feedback.text("Cold!");
+			} else if (difference>=20 && difference<30) {
+				feedback.text("Warm!");
+			} else if (difference>=10 && difference<20) {
+				feedback.text("Hot!");
+			} else {
+				feedback.text("Very Hot!");
+			}
 
-	  			$("#guessList").append("<li class='guessHistory'>" + userGuess + "</li>");
-	  			numGuess++;
-	  			$("#count").text(numGuess);
-	  		};
+			$("#guessList").append("<li class='guessHistory'>" + userGuess + "</li>");
+			numGuess++;
+			$("#count").text(numGuess);
+		};
 
 
 		/*--- Start a new game ---*/
-	$(".new").click(function() {
+		$(".new").click(function() {
 			reset();
-			random();	
+			random();
 			/*---Guessing a number---*/
-	  		$("#guessButton").click(function() {
+			$("#guessButton").click(function() {
 				guessingGame();
 			});
+			$(document).keypress(function(e) {
+				if(e.which == 13) {
+					guessingGame();
+				}
+			});
 		
-  	});
-	
-   	
-
-	
+		});
 });
 
 	
